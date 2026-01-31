8b17b6d70286e709f359d8fd895260cecbfafbb2
xiayuan <xiayuanovo@163.com>
2026-01-31 18:17:40 +0800

    fix(header): 修复站点信息

--- components/tabs.php
+++ components/tabs.php
@@ -1,6 +1,6 @@
 <?php
 $isCategory = $this->is('category');
-$this->widget('Widget_Metas_Category_List')->to($category); 
+$category = Typecho_Widget::widget('Widget_Metas_Category_List'); 
 ?>
 
 <div class="tab-list">
@@ -8,7 +8,7 @@ $this->widget('Widget_Metas_Category_List')->to($category);
         <a href="<?php $this->options->siteUrl(); ?>">全部</a>  
     </div>
     <?php while ($category->next()): ?>
-        <div class="tab-item<?php if ($isCategory && $this->getArchiveSlug() == $category->slug): ?> active<?php endif; ?>">
+        <div class="tab-item <?php if ($isCategory && $this->getArchiveSlug() == $category->slug): ?>active<?php endif; ?>">
             <a href="<?php $category->permalink(); ?>"><?php $category->name(); ?></a>
         </div>
     <?php endwhile; ?>
--- header.php
+++ header.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php echo $this->options->language; ?>">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title><?php $this->archiveTitle([
+        'category' => _t('分类 %s 下的文章'),
+        'search' => _t('包含关键字 %s 的文章'),
+        'tag' => _t('标签 %s 下的文章'),
+        'author' => _t('%s 发布的文章')
+    ], '', ' - '); ?><?php $this->options->title(); ?></title>
     <link rel="stylesheet" href="<?php $this->options->themeUrl('assets/css/index.css'); ?>">
 </head>
 <body>
