1d910ec181686fb68bc1262bf6fa3f5190a1a801
xiayuan <xiayuanovo@163.com>
2026-01-31 22:18:42 +0800

    feat(header): 新增手机头部样式

--- index.php
+++ index.php
@@ -14,7 +14,14 @@ $this->need('header.php');
 ?>
 <div class="page-container">
     <header class="page-header" style="background-image: url(<?php echo $this->options->headerImage; ?>);">
-
+        <div class="page-header__content">
+            <div class="page-header__avatar">
+                <img src="<?php echo htmlspecialchars(getAuthorAvatar($this->author->mail, $this->options)); ?>"
+                    alt="<?php echo htmlspecialchars($this->author->name); ?>">
+            </div>
+            <div class="page-header__title"><?php $this->options->title(); ?></div>
+            <div class="page-header__description"><?php $this->options->description(); ?></div>
+        </div>
     </header>
     <main class="page-main">
         <?php $this->need('components/sidebar.php'); ?>
