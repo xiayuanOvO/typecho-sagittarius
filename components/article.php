d0e5f0bbd09293bd9d2da8395b77f3c575cc0751
xiayuan <xiayuanovo@163.com>
2026-02-02 00:53:31 +0800

    feat: 添加文章页面及相关组件

---
+++ components/article.php
@@ -0,0 +1,23 @@
+<article class="post-item">
+    <!-- 文章侧边栏 -->
+    <div class="post__sidebar">
+        <a class="author__avatar" href="<?php $this->author->permalink(); ?>"
+            title="<?php $this->author(); ?>">
+            <img loading="lazy" src="<?php echo htmlspecialchars(getAuthorAvatar($this->author->mail, $this->options)); ?>"
+                alt="<?php echo htmlspecialchars($this->author->name); ?>">
+        </a>
+    </div>
+    <div class="post__main">
+        <div class="post__meta">
+            <div class="meta__name">
+                <?php $this->author(); ?>
+            </div>
+            <a class="meta__date" href="<?php $this->permalink(); ?>" time="<?php echo $this->created; ?>">
+                <?php echo $this->date('Y-m-d H:i'); ?>
+            </a>
+        </div>
+        <div class="post__content">
+            <?php $this->content(); ?>
+        </div>
+    </div>
+</article>
---
+++ post.php
@@ -0,0 +1,24 @@
+<?php $this->need('header.php'); ?>
+
+<div class="page-container">
+    <?php $this->need('components/header.php'); ?>
+    <main class="page-main">
+        <?php $this->need('components/sidebar.php'); ?>
+        <div class="page-content">
+            <div class="post__header">
+                <div class="header__back">
+                    <?php echo getSvg('left', 'back__icon'); ?>
+                    <a class="back__link" href="<?php $this->options->siteUrl(); ?>">返回</a>
+                </div>
+                <span class="header__title"><?php $this->title(); ?></span>
+            </div>
+            <?php $this->need('components/article.php'); ?>
+        </div>
+    </main>
+</div>
+
+<?php $this->need('footer.php'); ?>
+
+<script>
+    window.sagittarius.formatDate();
+</script>
